a43d68dc14df7b555bc40eb1151021cad657dcf2
selis <selisha@stu.usc.edu.tt>
2021-02-09 08:21:27 -0400

    2nd commit

--- src/Projectone.java
+++ src/Projectone.java
@@ -1,30 +1,124 @@
 import java.util.Scanner;
 
 public class Projectone {
-    public static void main (String[]args){
-        int MinsU, AccNmber;
 
-        Scanner sc= new Scanner(System.in);
+
+    public static void main (String[]args) {
+        int MinsU = 0, AccNmber; //MinsU: Minutes used, AccNmber:Account number
+        int PremExtMins; //Premium Extra Minutes
+        int RegExtMins ; //Regular Extra Minutes
+        double RegPMin=.20; //Regular per minutes (per cost of minutes over 50)
+        double PremPMinD =.10;  //Premium per Minutes Daily
+        double PremPMinN= .05; //Premium per Minutes Nights
+        double TotCost=0; //Total Cost
+        double TotPCost=0; //Total Premium Cost
+        int RegMinsFree = 50;   // Regular Minutes
+        int PremDMinsFree= 75; //Premium Daily Minutes Free
+        int PremNMinsFree= 100; // Premium Night Minutes Free
+        float RegCost = 10; //Regular Cost
+        float PremCost = 25; //Premium Cost
+        int DailyMin; // Daily Minutes
+
+        Scanner sc = new Scanner(System.in);
+        try{
         System.out.println("Please enter your account number: ");
-        AccNmber= sc.nextInt();
-
-        System.out.println("Please enter the amount of minutes used");
-        MinsU=sc.nextInt();
+        AccNmber = sc.nextInt();
 
         System.out.print("Please choose the correct letter for your plan: \na: Regular \nb: Premium \n");
         String plan= sc.nextLine();
-        char option= sc.nextLine().toUpperCase().toLowerCase(). charAt(0);
+        char option = sc.nextLine().toUpperCase().toLowerCase().charAt(0);
 
         switch (option) {
-            case 'a'-> System.out.println("You chose the Regular plan.");
-            case 'b'->System.out.println("You choose the Premium plan.");
-            default->System.out.println("Error please type a for the regular plan OR b for the premium plan");
-
+            case 'a' -> {
+                System.out.println("You chose the Regular plan.");
+                System.out.println("Please enter the amount of minutes service was used ");
+                MinsU = sc.nextInt();
+            }
+            case 'b' ->
+                System.out.println("You chose the Premium plan.");
+            default -> System.out.println("Error please type a for the regular plan OR b for the premium plan");
         }
-        System.out.println(""+"");
-        System.out.println("Account Number: "+AccNmber);
-        System.out.println("Minutes used: "+MinsU);
+
+
+
+        if (option == 'a' && MinsU <= 50) {
+            TotCost=RegCost;
+            System.out.println(""+"");
+            System.out.println("Account Number: " + AccNmber);
+            System.out.println("You chose the Regular plan");
+            System.out.println("The amount of minutes used: "+MinsU);
+            System.out.println("The cost of your service is $"+TotCost);
+        } else if (option == 'a') {
+            RegExtMins = MinsU - RegMinsFree;
+            TotCost = ((RegExtMins * RegPMin) + RegCost);
+            System.out.println(""+"");
+            System.out.println("Account Number: " + AccNmber);
+            System.out.println("You chose the Regular plan");
+            System.out.println("The amount of minutes used: "+MinsU);
+            System.out.println("The cost of your service is $"+TotCost);
+        }
+        else
+            System.out.println("What are the number of minutes the service was used during the day (6am-6pm) \n");
+               DailyMin = sc.nextInt();
+                System.out.println("What are the number of minutes the service was used during the night (6pm-6am) \n");
+                int NightMin = sc.nextInt();
+
+
+                if (DailyMin>75){
+                    PremExtMins=DailyMin-PremDMinsFree;
+                    TotPCost = ((PremExtMins * PremPMinD) + PremCost);
+                    System.out.println("The total cost of your selected service is: " +TotPCost);
+                }
+                else if ( NightMin>100){
+                PremExtMins=NightMin-PremNMinsFree;
+                TotPCost = ((PremExtMins * PremPMinN) + PremCost);
+                System.out.println("The total cost of your selected service is: " +TotPCost);
+                }
+
+                else if (DailyMin<=75 && NightMin<=100) {
+                    TotPCost = PremCost;
+                    System.out.println("The total cost of your service is: $" + TotPCost);
+                }
+
+                else if (DailyMin<=75 && NightMin>100){
+                PremExtMins=NightMin-PremNMinsFree;
+                TotPCost = ((PremExtMins * PremPMinN) + PremCost);
+                System.out.println("The Total cost of your service is: "+TotPCost);
+                 }
+
+                else if (DailyMin>75 && NightMin<=100){
+                PremExtMins=DailyMin-PremDMinsFree;
+                TotPCost = ((PremExtMins * PremPMinD) + PremCost);
+                System.out.println("The Total cost of your service is: "+TotPCost);
+                }
+                if (option=='b') {
+
+                    System.out.println("" +
+                            "");
+                    System.out.println("Account Number: " + AccNmber);
+                    System.out.println("Type of service: Premium");
+                    MinsU = DailyMin + NightMin;
+                    System.out.println("The amount of minutes used: " + MinsU);
+                    System.out.println("The cost of your service is $" + TotPCost);
+                }
+
+
+            }
+            catch(Exception ex){
+            System.out.println(ex);
+            }
+
 
     }
 
+
 }
+
+
+
+
+
+
+
+
+
---
+++ src/QuestionTwo.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class QuestionTwo {
+    public static void main (String[]args) {
+
+        Random ran = new Random();
+        int rannum = ran.nextInt(101);
+        int num;
+        int count = 0;
+        int limit = 5;
+        Scanner sc = new Scanner(System.in);
+
+
+
+            do {
+                System.out.println("Please input number: ");
+                num = sc.nextInt();
+                count++;
+
+                if (rannum == num) {
+                    System.out.println("You won");
+                } else if (rannum > num) {
+                    System.out.println("The number is higher");
+                } else
+                    System.out.println("The number is lower");
+            }while(count<limit);
+
+            System.out.println("You Lose");
+
+    }
+}
